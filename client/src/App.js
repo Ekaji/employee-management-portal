c2e431c20ae7de5d92c0bfce89c0cf758ea23c19
ekaji <ekajionah@gmail.com>
2021-10-04 23:49:51 +0100

    first heroku commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch('/api')
+    .then((res) => res.json())
+    .then((data) => setData(data.message))
+  },[])
+
     return (
         <div className="App">
-            <h1>hello world</h1>
+            <h1>{!data ? 'loading...' : data}</h1>
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
+const path = require('path')
 const PORT = 3001
 
-app.get('/', (req, res) => {
-    res.send(`<h1>hello world<h1>`)
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+
+app.get('/api', (req, res) => {
+    res.json({message: 'hello world 0'})
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 })
 
 app.listen(PORT, () => {
