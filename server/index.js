665ecab70d6a253ee15e4d5f8dd4aec4c0bddefb
ekaji <ekajionah@gmail.com>
2021-10-05 00:27:32 +0100

    package changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 
 app.use(express.static(path.resolve(__dirname, '../client/build')))
 
