bf3b89ade4d1b950415b5d2b6ed97fbd9515c0d4
ekaji <ekajionah@gmail.com>
2021-10-06 00:23:24 +0100

    fixed path to build folder in server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3001
 
-app.use(express.static(path.resolve(__dirname, '../client/build')))
+app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.get('/api', (req, res) => {
     res.json({message: 'hello world 0'})
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
 })
 
 app.listen(PORT, () => {
