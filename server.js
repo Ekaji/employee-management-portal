fe84f0b4fd0186c71ffeea00614abbf77d610b27
ekaji <ekajionah@gmail.com>
2021-10-09 00:54:08 +0100

    setup: prettier and eslint

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const PORT = process.env.PORT || 3001
+const express = require("express");
 
-app.use(express.static(path.resolve(__dirname, './client/build')))
+const app = express();
+const path = require("path");
 
-app.get('/api', (req, res) => {
-    res.json({message: 'hello world 0'})
-})
+const PORT = process.env.PORT || 3001;
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-})
+app.get("/api", (req, res) => {
+  res.json({ message: "hello world 0" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`)
-})
+  console.log(`server is running on ${PORT}`);
+});
